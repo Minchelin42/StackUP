a238c33939db55d0a44d4ddd605795e717f98dc9
Minchelin42 <mje0211@naver.com>
2024-05-14 12:26:08 +0900

    Initial Commit

---
+++ TechMasterSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TechMasterSwiftUI
+//
+//  Created by 민지은 on 2024/05/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TechMasterSwiftUI/TechMasterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechMasterSwiftUIApp.swift
+//  TechMasterSwiftUI
+//
+//  Created by 민지은 on 2024/05/14.
+//
+
+import SwiftUI
+
+@main
+struct TechMasterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

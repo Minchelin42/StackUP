71322e767e9002f4364fcd85c2d283f1c17ba3ea
Minchelin42 <mje0211@naver.com>
2024-05-15 15:06:08 +0900

    DESIGN: MyColor, MyImage 추가

---
+++ TechMasterSwiftUI/Style/MyColor.swift
@@ -0,0 +1,19 @@
+//
+//  MyColor.swift
+//  TechMasterSwiftUI
+//
+//  Created by 민지은 on 2024/05/14.
+//
+
+import SwiftUI
+
+enum MyColor {
+    
+    static let main = Color("hotPink")
+    static let darkGray = Color("darkGray")
+    static let lightGray = Color("lightGray")
+    static let white = Color.white
+    static let black = Color.black
+    
+    
+}
---
+++ TechMasterSwiftUI/Style/MyImage.swift
@@ -0,0 +1,15 @@
+//
+//  MyImage.swift
+//  TechMasterSwiftUI
+//
+//  Created by 민지은 on 2024/05/15.
+//
+
+import SwiftUI
+
+enum MyImage {
+    
+    static let scrap = Image("scrap")
+    static let testImg = Image("testThumbnail")
+    
+}

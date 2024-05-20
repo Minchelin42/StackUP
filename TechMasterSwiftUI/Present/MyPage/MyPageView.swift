605f849a674f696ffb9fd7890958f118c6f37390
Minchelin42 <mje0211@naver.com>
2024-05-20 23:29:48 +0900

    FEAT: MyPageView에서 MyPage로 case 분리

--- TechMasterSwiftUI/Present/MyPage/MyPageView.swift
+++ TechMasterSwiftUI/Present/MyPage/MyPageView.swift
@@ -7,31 +7,32 @@
 
 import SwiftUI
 
+enum MyPage: String, CaseIterable, Hashable {
+    case scrap = "찜한 강의"
+    case course = "수강신청 내역"
+    case customerService = "고객센터"
+}
+
 struct MyPageView: View {
     var body: some View {
-        VStack(alignment: .leading) {
+        NavigationView {
             List {
                 Section() {
-                    HStack {
-                        MyImage.testImg
-                            .resizable()
-                            .clipShape(Circle())
-                            .frame(width: 70, height: 70)
-                        
-                        VStack(alignment: .leading, spacing: 4) {
-                            Text("samdasu").blackMediumFont(size: 15)
-                            Text("프로필 수정").mainMediumFont(size: 13)
-                        }
-                        .padding(.leading, 8)
-                        Spacer()
+                    NavigationLink {
+                        NavigationLazyView(ContentView())
+                    } label: {
+                        ProfileView(nick: "삼다수")
                     }
-                    .padding(.vertical, 4)
                 }
                 
                 Section() {
-                    Text("찜한 강의").blackMediumFont(size: 13)
-                    Text("수강신청 내역").blackMediumFont(size: 13)
-                    Text("고객센터").blackMediumFont(size: 13)
+                    ForEach(MyPage.allCases, id: \.self) { myPage in
+                        NavigationLink {
+                            destination(myPage)
+                        } label: {
+                            Text(myPage.rawValue).blackMediumFont(size: 13)
+                        }
+                    }
                 }
                 
                 Section(header: Text("작성한 후기")) {
@@ -40,11 +41,48 @@ struct MyPageView: View {
                     MyReviewRow(action: { print("reviewRow Tapped") })
                     MyReviewRow(action: { print("reviewRow Tapped") })
                 }.listRowInsets(EdgeInsets.init(top: 4, leading: 8, bottom: 4, trailing: 4))
-
+                
             }.listSectionSpacing(12)
         }
     }
 }
+
+struct ProfileView: View {
+    
+    let nick: String
+    
+    var body: some View {
+        HStack {
+            MyImage.testImg
+                .resizable()
+                .clipShape(Circle())
+                .frame(width: 70, height: 70)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(nick).blackMediumFont(size: 15)
+                Text("프로필 수정").mainMediumFont(size: 13)
+            }
+            .padding(.leading, 8)
+            Spacer()
+        }
+        .padding(.vertical, 4)
+    }
+}
+
+extension MyPageView {
+    @ViewBuilder
+    private func destination(_ destination: MyPage) -> some View {
+        switch destination {
+        case .scrap:
+            NavigationLazyView(MyClassListView())
+        case .course:
+            NavigationLazyView(MyClassListView())
+        case .customerService:
+            ContentView() //나중에 채팅 뷰로 연결
+        }
+    }
+}
+
 
 #Preview {
     MyPageView()
